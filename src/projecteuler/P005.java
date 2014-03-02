482ca758f27713c2c5ff043c397114f2c03fe98a
Nonexistent <chunkiangvinegar@gmail.com>
2014-03-01 18:36:37 -0700

    fix

--- src/projecteuler/P005.java
+++ src/projecteuler/P005.java
@@ -5,7 +5,6 @@ public class P005 implements P {
 	@Override
 	public void g() {
 		int answer;
-		long s = System.nanoTime();
 		for(answer = 20; answer < Integer.MAX_VALUE; answer+=2){
 			int count = 0;
 			for(int i = 2; (answer % i == 0) && i <= 20; i++){
@@ -15,7 +14,6 @@ public class P005 implements P {
 				break;
 			}
 		}
-		System.out.println((System.nanoTime() - s)/1000000D);
 		System.out.println(answer);
 	}
 
